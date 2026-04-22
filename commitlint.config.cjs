db9d2e1f7c7d6f79788a810c7c20120eff7a6049
chenyiren <cyr@chenyirendeMac-mini.local>
2026-04-22 19:31:40 +0800

    feat: 中文补充

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,7 @@
+const { defineConfig } = require('cz-git')
+
 /** @type {import('@commitlint/types').UserConfig} */
-module.exports = {
+module.exports = defineConfig({
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
@@ -20,4 +22,67 @@ module.exports = {
       ],
     ],
   },
-}
+  prompt: {
+    alias: { fd: '文档: 修复错别字' },
+    messages: {
+      type: '选择你要提交的类型 :',
+      scope: '选择一个提交范围（可选）:',
+      customScope: '请输入自定义的提交范围 :',
+      subject: '填写简短精炼的变更描述 :\n',
+      body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+      breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+      footerPrefixesSelect: '选择关联issue前缀（可选）:',
+      customFooterPrefix: '输入自定义issue前缀 :',
+      footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+      generatingByAI: '正在通过 AI 生成你的提交简短描述...',
+      generatedSelectByAI: '选择一个 AI 生成的简短描述:',
+      confirmCommit: '是否提交或修改commit ?',
+    },
+    types: [
+      { value: 'feat', name: 'feat:     新增功能（特性）' },
+      { value: 'fix', name: 'fix:      修复缺陷' },
+      { value: 'docs', name: 'docs:     文档变更' },
+      { value: 'style', name: 'style:    代码格式（不影响功能）' },
+      { value: 'refactor', name: 'refactor: 代码重构（非修复/非新增）' },
+      { value: 'perf', name: 'perf:     性能优化' },
+      { value: 'test', name: 'test:     测试变更' },
+      { value: 'build', name: 'build:    构建流程/依赖变更' },
+      { value: 'ci', name: 'ci:       CI 配置/脚本变更' },
+      { value: 'revert', name: 'revert:   回滚提交' },
+      { value: 'chore', name: 'chore:    杂项（工具/流程等）' },
+    ],
+    useEmoji: false,
+    emojiAlign: 'center',
+    useAI: false,
+    aiNumber: 1,
+    themeColorCode: '',
+    scopes: [],
+    allowCustomScopes: true,
+    allowEmptyScopes: true,
+    customScopesAlign: 'bottom',
+    customScopesAlias: '以上都不是？我要自定义',
+    emptyScopesAlias: '跳过',
+    upperCaseSubject: null,
+    markBreakingChangeMode: false,
+    allowBreakingChanges: ['feat', 'fix'],
+    breaklineNumber: 100,
+    breaklineChar: '|',
+    skipQuestions: [],
+    issuePrefixes: [
+      // 如果使用 gitee 作为开发管理
+      { value: 'link', name: 'link:     链接 ISSUES 进行中' },
+      { value: 'closed', name: 'closed:   标记 ISSUES 已完成' },
+    ],
+    customIssuePrefixAlign: 'top',
+    emptyIssuePrefixAlias: '跳过',
+    customIssuePrefixAlias: '自定义前缀',
+    allowCustomIssuePrefix: true,
+    allowEmptyIssuePrefix: true,
+    confirmColorize: true,
+    scopeOverrides: undefined,
+    defaultBody: '',
+    defaultIssues: '',
+    defaultScope: '',
+    defaultSubject: '',
+  },
+})
